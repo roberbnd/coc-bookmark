c98da9029e8c01d7de0fb9ffe49d909c40310dce
voldikss <dyzplus@gmail.com>
2019-10-20 23:42:11 +0800

    Update README

---
+++ README.md
@@ -0,0 +1,88 @@
+# coc-bookmark
+
+Bookmark manager extension for [coc.nvim](https://github.com/neoclide/coc.nvim)
+
+Featured as [vim-bookmarks](https://github.com/MattesGroeger/vim-bookmarks)
+
+![](https://user-images.githubusercontent.com/20282795/67162033-f0aaa200-f392-11e9-9421-edff0893fe60.png)
+
+## Install
+
+```
+:CocInstall coc-bookmark
+```
+
+## Features
+
+- Toggle bookmark for a line
+- Add annotation for a line
+- Navigate all bookmarks with CocList
+- Bookmark will be deleted if the line was changed
+
+## Configuration
+
+```jsonc
+"bookmark.enable": {
+    "type": "boolean",
+    "default": true,
+    "description": "Whether to enable this extension"
+},
+"bookmark.maxsize": {
+    "type": "number",
+    "default": 5000,
+    "description": "Maxsize of bookmark"
+},
+"bookmark.sign": {
+    "type": "string",
+    "default": "⚑",
+    "description": "Bookmark icon for sign column"
+},
+"bookmark.signFg": {
+    "type": "string",
+    "default": "",
+    "description": "foreground color of bookmark sign"
+},
+"bookmark.signBg": {
+    "type": "string",
+    "default": "",
+    "description": "background color of bookmark sign"
+}
+```
+
+more information, see [package.json](https://github.com/voldikss/coc-bookmark/blob/master/package.json)
+
+## Commands
+
+- `:CocCommand bookmark.toggle`: create/delete a bookmark
+- `:CocCommand bookmark.annotate`: create a bookmark with annotation
+
+## Keymaps
+
+- Normal mode: `<Plug>(coc-bookmark-next)`
+- Normal mode: `<Plug>(coc-bookmark-prev)`
+- Normal mode: `<Plug>(coc-bookmark-toggle)`
+- Normal mode: `<Plug>(coc-bookmark-annotate)`
+
+```vim
+nmap <Leader>bj <Plug>(coc-bookmark-next)
+nmap <Leader>bk <Plug>(coc-bookmark-prev)
+```
+
+## CocList
+
+run `:CocList bookmark` to open the bookmark
+
+- Filter your bookmarks and perform operations via `<Tab>`
+- Use `preview` to preview the line you have marked
+- Use `delete` to delete a bookmark
+- Use `o` to jump to the line at the file
+
+## F.A.Q
+
+Q: Where are the bookmark data stored?
+
+A: Normally the data is saved in `~/.config/coc/extensions/coc-bookmark-data`, but if you set `g:coc_extension_root` to another location, it will change as well
+
+## License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,108 @@
+import {
+  commands,
+  ExtensionContext,
+  listManager,
+  workspace,
+  events
+} from 'coc.nvim'
+import { mkdirAsync, statAsync } from './util/io'
+import DB from './util/db'
+import BookmarkList from './lists/bookmark'
+import Bookmark from './commands'
+
+export async function activate(context: ExtensionContext): Promise<void> {
+  const config = workspace.getConfiguration('bookmark')
+  const enable = config.get<boolean>('enable', true)
+  if (!enable)
+    return
+
+  const { subscriptions, storagePath } = context
+  const { nvim } = workspace
+
+  const maxsize = config.get<number>('maxsize', 5000)
+  const db = new DB(storagePath, 'bookmark', maxsize)
+  const bookmark = new Bookmark(nvim, db)
+
+  const stat = await statAsync(storagePath)
+  if (!stat || !stat.isDirectory()) {
+    await mkdirAsync(storagePath)
+  }
+
+  const sign = config.get<string>('sign', '⚑')
+  const signFg = config.get<string>('signFg', '')
+  const signBg = config.get<string>('signBg', '')
+  await nvim.command('hi def link BookMarkHI Identifier')
+  if (signFg)
+    await nvim.command(`hi BookMarkHI guifg=${signFg}`)
+  if (signBg)
+    await nvim.command(`hi BookMarkHI guibg=${signBg}`)
+  nvim.command(`sign define BookMark text=${sign} texthl=BookMarkHI`, true)
+
+  workspace.onDidOpenTextDocument(async () => {
+    await bookmark.updateSign()
+  }, null, subscriptions)
+
+  workspace.onDidChangeTextDocument(async () => {
+    await bookmark.updateSign()
+  }, null, subscriptions)
+
+  events.on('CursorHold', async () => {
+    await bookmark.updateSign()
+  }, null, subscriptions)
+
+  events.on('BufEnter', async () => {
+    await bookmark.updateSign()
+  }, null, subscriptions)
+
+  subscriptions.push(
+    workspace.registerKeymap(
+      ['n'],
+      'bookmark-toggle',
+      async () => await bookmark.toggle(),
+      { sync: false }
+    ))
+
+  subscriptions.push(
+    workspace.registerKeymap(
+      ['n'],
+      'bookmark-annotate',
+      async () => await bookmark.annotate(),
+      { sync: false }
+    ))
+
+  subscriptions.push(
+    workspace.registerKeymap(
+      ['n'],
+      'bookmark-next',
+      async () => await bookmark.find('next'),
+      { sync: false }
+    ))
+
+  subscriptions.push(
+    workspace.registerKeymap(
+      ['n'],
+      'bookmark-prev',
+      async () => await bookmark.find('prev'),
+      { sync: false }
+    ))
+
+  subscriptions.push(
+    commands.registerCommand(
+      'bookmark.toggle',
+      async () => await bookmark.toggle()
+    )
+  )
+
+  subscriptions.push(
+    commands.registerCommand(
+      'bookmark.annotate',
+      async () => await bookmark.annotate()
+    )
+  )
+
+  subscriptions.push(
+    listManager.registerList(
+      new BookmarkList(nvim, db)
+    )
+  )
+}
